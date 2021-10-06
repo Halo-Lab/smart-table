78ab0c7ad46c8ef42ddc062faf3088198e7e37aa
Quintis1212 <mataleo1212@gmail.com>
2021-10-06 12:05:43 +0300

    new script for file update added

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const hound = require('hound')
+const { execSync } = require('child_process');
 
+function injectScript() {
+    execSync(' npm run build')
+    let projectFolder;
+
+    // get project folder name
+    fs.readdirSync('../').forEach(file => {
+        if(file !== 'smart-table'){
+            projectFolder=file
+        }
+      });
+    
+    //copy file to lib folder project
+    fs.copyFile('./lib/index.js', `../${projectFolder}/node_modules/react-smart-dynamic-table/lib/index.js`, (err) => {
+        if (err) throw err;
+        console.log('\x1b[33m%s\x1b[0m','Script was injected to project !');
+    });
+}
+
+injectScript()
+
+function startWatch(){
+
+   const watcher = hound.watch('./src');
+    watcher.on('change', function(file) {
+    console.log(file + ' was changed , run inject script ')
+    injectScript()
+  })
+
+}
+
+startWatch()
