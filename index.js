9b817bfc3777b06a59ced179832381917b979fc8
Quintis1212 <mataleo1212@gmail.com>
2021-10-07 09:24:34 +0300

    html markup , folder checks

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-const fs = require('fs');
-const hound = require('hound')
-const { execSync,exec } = require('child_process');
+const fs = require("fs");
+const hound = require("hound");
+const { execSync, exec } = require("child_process");
 
 function injectScript() {
-    execSync(' npm run build')
-    let projectFolder= null;
+  execSync(" npm run build");
+  let projectFolder = null;
 
-    // get project folder name
-    fs.readdirSync('../').forEach(file => {
-        if(file !== 'smart-table' && projectFolder === null){
-            projectFolder=file;
-        } else if (projectFolder) {
-          throw new Error('Too many folders  !');
-        }
-      });
-
-    if(projectFolder === null) {
-       throw new Error('Can not find project folder !')
+  // get project folder name
+  fs.readdirSync("../").forEach((file, i, fileArray) => {
+    if (fileArray.length > 2) {
+      throw new Error("Too many folders !");
     }
-      //copy file to lib folder project
-    fs.copyFile('./lib/index.js', `../${projectFolder}/node_modules/react-smart-dynamic-table/lib/index.js`, (err) => {
-        if (err) throw err;
-        console.log('\x1b[33m%s\x1b[0m','Script was injected to project !');
-    });
+    if (fileArray.length === 1) {
+      throw new Error("Can not find project folder !");
+    }
+    if (file !== "smart-table" && projectFolder === null) {
+      projectFolder = file;
+    }
+  });
+
+  //copy file to lib folder project
+  fs.copyFile(
+    "./lib/index.js",
+    `../${projectFolder}/node_modules/react-smart-dynamic-table/lib/index.js`,
+    (err) => {
+      if (err) throw err;
+      console.log("\x1b[33m%s\x1b[0m", "Script was injected to project !");
+    }
+  );
 }
 
-injectScript()
+injectScript();
 
-function startWatch(){
+function startWatch() {
 
-   const watcher = hound.watch('./src');
-    watcher.on('change', function(file) {
-    console.log(file + ' was changed , run inject script ')
-    injectScript()
-  })
-
+  const watcher = hound.watch("./src");
+  watcher.on("change", function (file) {
+    console.log(file + " was changed , run inject script ");
+    injectScript();
+  });
 }
 
-startWatch()
+startWatch();
